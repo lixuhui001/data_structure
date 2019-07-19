55d4265e87ba6b084c0e4f089c182268dba2c0a3
lixuhui <lixuhui@gooagoo.com>
2019-07-19 09:33:10 +0800

    修改冒泡拍戏

--- lxh-data-structure/src/main/java/com/lxh/array/MyArray.java
+++ lxh-data-structure/src/main/java/com/lxh/array/MyArray.java
@@ -7,7 +7,8 @@ import java.util.Random;
  * @Date: 2019/7/16/016 09:30
  * @Description:
  */
-public class MyArray {
+public class MyArray
+{
 
     private final static int DEFAULT_SIZE = 50;
 
@@ -25,7 +26,8 @@ public class MyArray {
      * 默认构造函数
      * 定义一个长度为50的数组
      */
-    public MyArray() {
+    public MyArray()
+    {
         arr = new int[DEFAULT_SIZE];
     }
 
@@ -33,14 +35,16 @@ public class MyArray {
      * 有参构造函数
      * 定义自定义长度数组
      */
-    public MyArray(int max) {
+    public MyArray(int max)
+    {
         arr = new int[max];
     }
 
     /**
      * 有效数据计算，预留扩容数族
      */
-    private void expansionElem() {
+    private void expansionElem()
+    {
         this.elem++;
     }
 
@@ -48,7 +52,8 @@ public class MyArray {
      * 插入数据
      * 有效数+1
      */
-    public void insert(int value) {
+    public void insert(int value)
+    {
         arr[elem] = value;
         elem++;
     }
@@ -58,27 +63,36 @@ public class MyArray {
      * 每次插入都进行排序
      * 大于该数的依次后移
      */
-    public void orderInsert(int value) {
+    public void orderInsert(int value)
+    {
         int i;
-        for (i = 0; i < elem; i++) {
-            if (arr[i] > value) {
+        for (i = 0; i < elem; i++)
+        {
+            if (arr[i] > value)
+            {
                 break;
             }
         }
         elem++;
-        for (int j = elem; j > i; j--) {
+        for (int j = elem; j > i; j--)
+        {
             arr[j] = arr[j - 1];
         }
         arr[i] = value;
     }
 
-    public void delete(long deleteKey) {
+    public void delete(long deleteKey)
+    {
 
         int index;
-        if ((index = find(deleteKey)) == -1) {
+        if ((index = find(deleteKey)) == -1)
+        {
             System.out.println("数据不存在");
-        } else {
-            for (int i = index; i < elem; i++) {
+        }
+        else
+        {
+            for (int i = index; i < elem; i++)
+            {
                 //前移一位
                 arr[i] = arr[i + 1];
             }
@@ -89,8 +103,10 @@ public class MyArray {
     /**
      * 显示数据
      */
-    public void display() {
-        for (int i = 0; i < elem; i++) {
+    public void display()
+    {
+        for (int i = 0; i < elem; i++)
+        {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
@@ -99,10 +115,13 @@ public class MyArray {
     /**
      * 普通查找
      */
-    public int find(long searchKey) {
+    public int find(long searchKey)
+    {
         int i;
-        for (i = 0; i < elem; i++) {
-            if (arr[i] == searchKey) {
+        for (i = 0; i < elem; i++)
+        {
+            if (arr[i] == searchKey)
+            {
                 return i;
             }
         }
@@ -113,21 +132,31 @@ public class MyArray {
      * 二分法查找
      * 如数组有序,则可使用二分法查找
      */
-    public int binaryFind(int searchKey) {
+    public int binaryFind(int searchKey)
+    {
         int ins = 0;
         int low = 0;
         int pow = elem;
 
-        while (true) {
+        while (true)
+        {
             ins = (low + pow) / 2;
-            if (arr[ins] == searchKey) {
+            if (arr[ins] == searchKey)
+            {
                 return ins;
-            } else if (low > pow) {
+            }
+            else if (low > pow)
+            {
                 return -1;
-            } else {
-                if (arr[ins] > searchKey) {
+            }
+            else
+            {
+                if (arr[ins] > searchKey)
+                {
                     pow = ins - 1;
-                } else {
+                }
+                else
+                {
                     low = ins + 1;
                 }
             }
@@ -145,13 +174,18 @@ public class MyArray {
      * 优化算法： 判断是否存在提前结束排序循环：
      * 当内层循环未发票数据交换则表示顺序已经是从小到大，可以提交结束
      */
-    public void bubbleSort() {
+    public void bubbleSort()
+    {
         int tmp = 0;
         int flag = 1;
-        for (int i = 0; i < elem - 1; i++) {
+        //当且等于0的时候 标志着排序完毕
+        for (int i = 0; i < elem - 1 && flag == 1; i++)
+        {
             flag = 0;
-            for (int j = 0; j < elem - i - 1 && flag == 1; j++) {
-                if (arr[j] > arr[j + 1]) {
+            for (int j = 0; j < elem - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
                     flag = 1;
                     tmp = arr[j];
                     arr[j] = arr[j + 1];
@@ -168,12 +202,15 @@ public class MyArray {
      * 插入排序：
      * 从游标为1的数据开始选中，并记录该数，依次和前一位比较，如大于被选择数，则后移一位（被选择数位置已空开），如小于则将选中数放于当前循环位
      */
-    public void insertSort() {
+    public void insertSort()
+    {
         int select = 0;
-        for (int i = 1; i < elem; i++) {
+        for (int i = 1; i < elem; i++)
+        {
             select = arr[i];
             int j = 0;
-            for (j = i; j > 0 && arr[j - 1] >= select; j--) {
+            for (j = i; j > 0 && arr[j - 1] >= select; j--)
+            {
                 arr[j] = arr[j - 1];
             }
             arr[j] = select;
@@ -187,13 +224,17 @@ public class MyArray {
      * 每次循环均忽略上一轮参与循环的最首位（最小数）
      * 假设循环次数为i 则第I次开始被选择的下标为i，参与循环比较的数据为i+1到length。
      */
-    public void selectSort() {
+    public void selectSort()
+    {
         int min = 0;
         int tmp = 0;
-        for (int i = 0; i < elem - 1; i++) {
+        for (int i = 0; i < elem - 1; i++)
+        {
             min = i;
-            for (int j = i + 1; j < elem; j++) {
-                if (arr[j] < arr[min]) {
+            for (int j = i + 1; j < elem; j++)
+            {
+                if (arr[j] < arr[min])
+                {
                     min = j;
                 }
             }
@@ -205,25 +246,30 @@ public class MyArray {
 
     }
 
-    public void rundomInsert(int length) {
+    public void rundomInsert(int length)
+    {
         Random random = new Random();
 
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++)
+        {
             insert(random.nextInt(1000));
         }
 
     }
 
-    public void rundomorderInsert(int length) {
+    public void rundomorderInsert(int length)
+    {
         Random random = new Random();
 
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++)
+        {
             orderInsert(random.nextInt(1000));
         }
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         MyArray myArray = new MyArray();
         myArray.rundomInsert(10);
         myArray.display();
